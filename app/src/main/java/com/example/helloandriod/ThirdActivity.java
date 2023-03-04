da15d8780e39da1ce4718025fd43039ea6a06438
EarlPaquibut <earlpaquibut@gmail.com>
2023-03-04 12:17:43 +0800

    Buttons and stuff

--- app/src/main/java/com/example/helloandriod/ThirdActivity.java
+++ app/src/main/java/com/example/helloandriod/ThirdActivity.java
@@ -1,8 +1,14 @@
 package com.example.helloandriod;
 
+import static com.example.helloandriod.MainActivity.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class ThirdActivity extends AppCompatActivity {
 
@@ -10,5 +16,17 @@ public class ThirdActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Log.i(TAG, "Button click!");
+                Intent intent = new Intent( ThirdActivity.this, helloandriod.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/helloandriod/helloandriod.java
@@ -0,0 +1,16 @@
+package com.example.helloandriod;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class helloandriod extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_helloandriod);
+
+
+    }
+}
