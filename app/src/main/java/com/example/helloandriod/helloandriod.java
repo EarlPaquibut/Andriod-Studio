5402ab8db47a210575b67019b92bf0a2370499ed
EarlPaquibut <earlpaquibut@gmail.com>
2023-03-04 12:28:26 +0800

    Inputs and stuff

--- app/src/main/java/com/example/helloandriod/helloandriod.java
+++ app/src/main/java/com/example/helloandriod/helloandriod.java
@@ -3,14 +3,42 @@ package com.example.helloandriod;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class helloandriod extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class helloandriod<string> extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_helloandriod);
+        Spinner spinner = findViewById(R.id.spinner3);
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("JAVA");
+        arrayList.add("ANDROID");
+        arrayList.add("C Language");
+        arrayList.add("CPP Language");
+        arrayList.add("Go Language");
+        arrayList.add("AVN SYSTEMS");
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, arrayList);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(arrayAdapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                //string tutorialsName = parent.getItemAtPosition(position).toString();
+                //Toast.makeText(parent.getContext(), "Selected: " + tutorialsName,Toast.LENGTH_LONG).show();
+            }
 
-
+            @Override
+            public void onNothingSelected(AdapterView <?> parent) {
+            }
+        });
     }
 }
+
