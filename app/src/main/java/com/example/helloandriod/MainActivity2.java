7533c904537d22492840e094c020e55e63a86ffa
EarlPaquibut <earlpaquibut@gmail.com>
2023-03-11 11:38:01 +0800

    loadFragment for A and B

---
+++ app/src/main/java/com/example/helloandriod/MainActivity2.java
@@ -0,0 +1,44 @@
+package com.example.helloandriod;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    Button firstfragment, secondfragment;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        firstfragment = (Button) findViewById(R.id.firstfragment);
+        secondfragment = (Button) findViewById(R.id.secondfragment);
+
+        firstfragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new FragmentA());
+            }
+        });
+        secondfragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new FragmentB());
+            }
+        });
+    }
+    private void loadFragment(Fragment fragment){
+
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
+
+
+    }
+}
