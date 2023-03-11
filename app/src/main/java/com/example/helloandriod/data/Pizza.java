7441625e3aec3d05cbe20f0a12eb02cfe2c72acf
EarlPaquibut <earlpaquibut@gmail.com>
2023-03-11 13:05:14 +0800

    loadFragment,background color,Text, starting pizza Done for A and B

---
+++ app/src/main/java/com/example/helloandriod/data/Pizza.java
@@ -0,0 +1,4 @@
+package com.example.helloandriod.data;
+
+public class Pizza {
+}
---
+++ app/src/main/java/com/example/helloandriod/fragments/PizzaMenuFragment.java
@@ -0,0 +1,16 @@
+package com.example.helloandriod.fragments;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.helloandriod.R;
+
+public class PizzaMenuFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pizza_menu_fragment);
+    }
+}
